e36329e95efbb8adbb4b0c33d23c45fc16925a38
louis <louis@lemoncloud.io>
2025-09-18 15:38:20 +0900

    refactor(blog-post-page): remove hero image

--- src/components/blog-post-page.tsx
+++ src/components/blog-post-page.tsx
@@ -152,16 +152,16 @@ export default function BlogPostPage({ page, relatedPosts }: BlogPostPageProps) 
           </div>
 
           {/* Hero Image */}
-          <div className="relative h-[400px] rounded-xl overflow-hidden border border-border mb-8">
-            <Image
-              src="https://images.unsplash.com/photo-1620712943543-bcc4688e7485?q=80&w=1200&h=800&auto=format&fit=crop"
-              alt={page.data.title || "Blog post image"}
-              fill
-              className="object-cover"
-              priority
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent"></div>
-          </div>
+          {/*<div className="relative h-[400px] rounded-xl overflow-hidden border border-border mb-8">*/}
+          {/*  <Image*/}
+          {/*    src="https://images.unsplash.com/photo-1620712943543-bcc4688e7485?q=80&w=1200&h=800&auto=format&fit=crop"*/}
+          {/*    alt={page.data.title || "Blog post image"}*/}
+          {/*    fill*/}
+          {/*    className="object-cover"*/}
+          {/*    priority*/}
+          {/*  />*/}
+          {/*  <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent"></div>*/}
+          {/*</div>*/}
 
         </header>
 
